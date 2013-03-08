562719802356da70933d8f2572ea764b2c873666
Dennis Lorson <dennis@orbicule.com>
2013-03-08 10:40:44 +0100

    Fixed left/right generation for root node

--- lib/awesome_nested_set/awesome_nested_set.rb
+++ lib/awesome_nested_set/awesome_nested_set.rb
@@ -514,11 +514,20 @@ module CollectiveIdea #:nodoc:
         end
         
         # Recursively sets the left and right properties (in-memory) for this node and its descendants.
-        def set_in_memory_left_and_right(count = 1)
+        def set_in_memory_left_and_right(count = nil)
+          
+          # The lowest left should be larger than the highest right in the entire table!
+          unless count
+            highest_right_row = nested_set_scope(:order => "#{quoted_right_column_full_name} desc").limit(1).lock(true).first
+            maxright = highest_right_row ? (highest_right_row[right_column_name] || 0) : 0
+            count = maxright + 1
+          end
+          
           self.lft = count
           count += 1
           children.each { |c| count = c.set_in_memory_left_and_right(count) + 1 }
           self.rgt = count
+          
         end
         
 
